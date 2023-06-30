f15c5dd5aebb670212e12eb41d5fd44b98854871
Kharlamov39 <kharlamov39@yandex.ru>
2023-06-30 12:18:25 +0300

    added post fix

--- models/Post.js
+++ models/Post.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const PostSchema = new mongoose.Schema({
     text: {
         type: String,
-        required: true
     },
     image: {
         type: String,
+        required: true
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
